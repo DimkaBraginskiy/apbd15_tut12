c62f60d29dddb618398850d5a1d64a0da7b02b1f
Dmytro Brahinskyi <dbraginskiy821@gmail.com>
2025-06-07 00:05:11 +0200

    feat: Add minimal models

---
+++ apbd12/Data/ApbdContext.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using apbd12.Models;
+
+namespace apbd12.Data;
+
+public partial class ApbdContext : DbContext
+{
+    public ApbdContext()
+    {
+    }
+
+    public ApbdContext(DbContextOptions<ApbdContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    public virtual DbSet<ClientTrip> ClientTrips { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public virtual DbSet<Trip> Trips { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:Default");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.HasKey(e => e.IdClient).HasName("PK__Client__C1961B33130AC90A");
+
+            entity.ToTable("Client");
+
+            entity.Property(e => e.Email).HasMaxLength(120);
+            entity.Property(e => e.FirstName).HasMaxLength(120);
+            entity.Property(e => e.LastName).HasMaxLength(120);
+            entity.Property(e => e.Pesel).HasMaxLength(120);
+            entity.Property(e => e.Telephone).HasMaxLength(120);
+        });
+
+        modelBuilder.Entity<ClientTrip>(entity =>
+        {
+            entity.HasKey(e => new { e.IdClient, e.IdTrip }).HasName("PK__Client_T__C823521EBAC2D536");
+
+            entity.ToTable("Client_Trip");
+
+            entity.HasOne(d => d.IdClientNavigation).WithMany(p => p.ClientTrips)
+                .HasForeignKey(d => d.IdClient)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Client_Tr__IdCli__534D60F1");
+
+            entity.HasOne(d => d.IdTripNavigation).WithMany(p => p.ClientTrips)
+                .HasForeignKey(d => d.IdTrip)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Client_Tr__IdTri__5441852A");
+        });
+
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.HasKey(e => e.IdCountry).HasName("PK__Country__F99F104DCCFABF70");
+
+            entity.ToTable("Country");
+
+            entity.Property(e => e.Name).HasMaxLength(120);
+
+            entity.HasMany(d => d.IdTrips).WithMany(p => p.IdCountries)
+                .UsingEntity<Dictionary<string, object>>(
+                    "CountryTrip",
+                    r => r.HasOne<Trip>().WithMany()
+                        .HasForeignKey("IdTrip")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK__Country_T__IdTri__5070F446"),
+                    l => l.HasOne<Country>().WithMany()
+                        .HasForeignKey("IdCountry")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK__Country_T__IdCou__4F7CD00D"),
+                    j =>
+                    {
+                        j.HasKey("IdCountry", "IdTrip").HasName("PK__Country___F02A5960DD5447BD");
+                        j.ToTable("Country_Trip");
+                    });
+        });
+
+        modelBuilder.Entity<Trip>(entity =>
+        {
+            entity.HasKey(e => e.IdTrip).HasName("PK__Trip__9B5492D19EEDCDAB");
+
+            entity.ToTable("Trip");
+
+            entity.Property(e => e.DateFrom).HasColumnType("datetime");
+            entity.Property(e => e.DateTo).HasColumnType("datetime");
+            entity.Property(e => e.Description).HasMaxLength(220);
+            entity.Property(e => e.Name).HasMaxLength(120);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
